b74a157ad5aab0833b7bf44a1a67f7274e6a56c7
Jussi Pienisaari <jussi.pienisaari@insolution.fi>
2019-12-07 10:45:21 +0200

    Completed 2.7, started on 2.8

--- osa2/puhelinluettelo/src/App.js
+++ osa2/puhelinluettelo/src/App.js
@@ -2,28 +2,41 @@ import React, { useState } from 'react'
 
 const PersonList = ({persons}) => {
   const listItems = () =>
-    persons.map(person => <li key={person.name}>{person.name}</li>)
+    persons.map(person => <li key={person.name}>{person.name} {person.number}</li>)
   return (
     listItems()
   )
 }
 
 const App = () => {
-  const [ persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas', number: '040-123456' },
+    { name: 'Ada Lovelace', number: '39-44-5323523' },
+    { name: 'Dan Abramov', number: '12-43-234345' },
+    { name: 'Mary Poppendieck', number: '39-23-6423122' }
   ]) 
   const [ newName, setNewName ] = useState('')
+  const [ newNumber, setNewNumber ] = useState('')
 
-  const changeInput = (event) => {
+  const inputName = (event) => {
     setNewName(event.target.value)
+  }
+
+  const inputNumber = (event) => {
+
   }
 
   const onSubmit = (event) => {
     event.preventDefault()
-    const personObject= {
-      name: newName,
+    console.log(persons)
+    if (persons.find(person => person.name === newName)) {
+      alert(`${newName} is already added to phonebook!`)
+    } else {
+      const personObject= {
+        name: newName,
+      }
+      setPersons(persons.concat(personObject))
     }
-    setPersons(persons.concat(personObject))
     setNewName('')
   }
 
@@ -33,7 +46,11 @@ const App = () => {
       <form onSubmit={onSubmit}>
         <div>
           <label htmlFor="name">name</label>
-          <input type="text" name="name" onChange={changeInput} value={newName}/>
+          <input type="text" name="name" onChange={inputName} value={newName}/>
+        </div>
+        <div>
+          <label htmlFor="number">number</label>
+          <input type="text" name="number"onChange={inputNumber} value={newNumber}/>
         </div>
         <div>
           <button type="submit">add</button>

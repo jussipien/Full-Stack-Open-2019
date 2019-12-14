da7c21cec963b7ff5f2a6f8c08e9902bebaee333
Jussi Pienisaari <jussi.pienisaari@insolution.fi>
2019-12-14 19:40:15 +0200

    Made tests, created routes for deleting and updating by id

--- osa4/blogilista/controllers/blogs.js
+++ osa4/blogilista/controllers/blogs.js
@@ -10,12 +10,52 @@ blogsRouter.get('/', (request, response) => {
 })
   
 blogsRouter.post('/', (request, response) => {
-  const blog = new Blog(request.body)
+  let blog = new Blog(request.body)
+
+  if (!blog.title || !blog.url) {
+    response.status(400).end()
+  }
+
+  if (!blog.likes) {
+    blog.likes = 0
+  }
 
   blog
     .save()
     .then(result => {
       response.status(201).json(result)
+    })
+})
+
+blogsRouter.delete('/:id', async (request, response) => {
+  const id = request.params.id
+
+  await Blog
+    .findByIdAndDelete(id)
+    .then(result => {
+      response.status(204).end()
+    })
+    .catch(error => {
+      response.status(404).json({'error': 'no blog matching id found'})
+    })
+})
+
+blogsRouter.put('/:id', async (request, response) => {
+  const id = request.params.id
+  const body = request.body
+
+  if (!body.title || !body.url) {
+    response.status(400).end()
+  }
+
+  await Blog
+    .findByIdAndUpdate(id, body , { new: true } )
+    .then(result => {
+      response.status(200).json(result.toJSON())
+    })
+    .catch(error => {
+      console.log(error)
+      response.status(404).json({'error': 'no blog matching id found'})
     })
 })
 
--- osa4/blogilista/tests/blog_api.test.js
+++ osa4/blogilista/tests/blog_api.test.js
@@ -11,6 +11,47 @@ test('blogs are returned as json', async () => {
     .expect('Content-Type', /application\/json/)
 })
 
+test('identification field of blogs is "id"', async () => {
+  const res = await api.get('/api/blogs')
+
+  expect(res.body[0].id).toBeDefined()
+})
+
+test('new blogs can be added by posting to "/api/blogs"', async () => {
+  const resBefore = await api.get('/api/blogs')
+
+  await api
+    .post('/api/blogs')
+    .send({'title': 'test', 'author': 'test', 'url': 'test', 'likes': 10})
+    .set('Accept', 'application/json')
+    .expect(201)
+    .expect('Content-Type', /json/)
+
+    const resAfter = await api.get('/api/blogs')
+    
+    expect(resAfter.body.length).toBeGreaterThan(resBefore.body.length)
+})
+
+test('posting blog without likes property will fill it with 0', async () => {
+  const res = await api
+    .post('/api/blogs')
+    .send({'title': 'test', 'author': 'test', 'url': 'test'})
+
+    expect(res.body.likes).toBeLessThanOrEqual(0)
+})
+
+test('posting blog without title or url property will return status 400 ', async () => {
+    await api
+      .post('/api/blogs')
+      .send({'title': 'test', 'author': 'test', 'likes': 10})
+      .expect(400)
+
+    await api
+      .post('/api/blogs')
+      .send({'author': 'test', 'url': 'test', 'likes': 10})
+      .expect(400)
+})
+
 afterAll(() => {
   mongoose.connection.close()
 })

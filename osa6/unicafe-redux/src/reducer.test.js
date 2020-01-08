e3f0f36d547af4b42156fa5be64b3b629978d91f
Jussi Pienisaari <jussi.pienisaari@insolution.fi>
2020-01-08 19:41:49 +0200

    Completed tests for 6.1

--- osa6/unicafe-redux/src/reducer.test.js
+++ osa6/unicafe-redux/src/reducer.test.js
@@ -32,4 +32,54 @@ describe('unicafe reducer', () => {
       bad: 0
     })
   })
+
+  test('ok is incremented', () => {
+    const action = {
+      type: 'OK'
+    }
+    const state = initialState
+
+    deepFreeze(state)
+    const newState = counterReducer(state, action)
+    expect(newState).toEqual({
+      good: 0,
+      ok: 1,
+      bad: 0
+    })
+  })
+
+  test('bad is incremented', () => {
+    const action = {
+      type: 'BAD'
+    }
+    const state = initialState
+
+    deepFreeze(state)
+    const newState = counterReducer(state, action)
+    expect(newState).toEqual({
+      good: 0,
+      ok: 0,
+      bad: 1
+    })
+  })
+
+  test('clearing votes works', () => {
+    const action = {
+      type: 'ZERO'
+    }
+    const state = {
+      good: 10,
+      ok: 10,
+      bad: 10
+    }
+
+    deepFreeze(state)
+    const newState = counterReducer(state, action)
+    expect(newState).toEqual({
+      good: 0,
+      ok: 0,
+      bad: 0
+    })
+  })
+
 })
